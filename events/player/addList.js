5d550f3dad78b78902070efb47add6834027dfb5
Asif Sheikh <116940358+notasifoffficial@users.noreply.github.com>
2024-01-26 18:10:30 +0600

    Update addList.js

--- events/player/addList.js
+++ events/player/addList.js
@@ -5,17 +5,4 @@ queue?.textChannel?.send({ content: `<@${playlist.user.id}>, \`${playlist.name} 
 }
 
 /*
-
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
 */
